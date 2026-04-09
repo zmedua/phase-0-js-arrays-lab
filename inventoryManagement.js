647f7bb0b7bd8b689ffa26d11b2e4ce5cfaa552e
Zane <zimedua@gmail.com>
2026-04-09 11:28:53 -0500

    array lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,8 +1,35 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct()
+  {   
+      console.log(products[0]);
+  }
+logFirstProduct(products);
+
+ const addProduct = function( productName)
+  {
+    products.push(productName);
+  }
+addProduct(products,"bike");
 
 
+const updateProductName = function(position,newName)
+  {
+    products.splice(position, 1, newName);
+    return products;
+    
+  }
+  updateProductName(1,"Tablet");
+  
 
-// Export the necessary parts for testing
+  const removeLastProduct = function()
+    {
+      products.pop()
+    }
+    removeLastProduct(products);
+    
+//Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
